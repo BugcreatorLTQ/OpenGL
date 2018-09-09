ebb9997eabe0fc097406ae867753e0b4676fbe94
BugcreatorLTQ <bugcreator@qq.com>
2018-09-09 13:38:15 +0800

    Point

--- OpenGL/Point.h
+++ OpenGL/Point.h
@@ -1,6 +1,5 @@
 #ifndef POINT_H
 #define POINT_H
-#include<GL/glut.h>
 
 template <typename Type>
 class Point
@@ -13,6 +12,11 @@ public:
 	void SetPoint(const Type x, const Type y);
 	Type X() const;
 	Type Y() const;
+	Point & operator+=(const Type & other_point);
+	Point & operator-=(const Type & other_point);
+	Point operator+(const Type & other_point);
+	Point operator-(const Type & other_point);
+	Type operator*(const Type & other_point);
 };
 
 template <typename Type>
@@ -45,4 +49,43 @@ Type Point<Type>::Y() const
 {
 	return y;
 }
+
+template <typename Type>
+Point<Type> & Point<Type>::operator+=(const Type & other_point)
+{
+	x += other_point.x;
+	y += other_point.y;
+	return *this;
+}
+
+template <typename Type>
+Point<Type> & Point<Type>::operator-=(const Type & other_point)
+{
+	x -= other_point.x;
+	y -= other_point.y;
+	return *this;
+}
+
+template <typename Type>
+Point<Type> Point<Type>::operator+(const Type & other_point)
+{
+	Point result;
+	result.SetPoint(x + other_point.x, y + other_point.y);
+	return result;
+}
+
+template <typename Type>
+Point<Type> Point<Type>::operator-(const Type & other_point)
+{
+	Point result;
+	result.SetPoint(x - other_point.x, y - other_point.y);
+	return result;
+}
+
+template <typename Type>
+Type Point<Type>::operator*(const Type & other_point)
+{
+	return x * other_point.x + y * other_point.y;
+}
+
 #endif
