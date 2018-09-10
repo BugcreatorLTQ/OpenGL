d3ecee968a7e4fd4ede81cc9f733c62b4e38fcdb
BugcreatorLTQ <bugcreator@qq.com>
2018-09-10 12:10:02 +0800

    实验一完成

--- OpenGL/MyTool.h
+++ OpenGL/MyTool.h
@@ -7,28 +7,48 @@ template<typename Type>
 struct point {
 	Type x, y;
 	point & operator*=(const Type size);
-	point operator*(const Type size);
-	Type operator*(const point other);
+	point operator*(const Type size) const;
+	Type operator*(const point & other) const;
+	point & operator+=(const point & other);
+	point operator+(const point & other) const;
 };
 
 template<typename Type>
-point<Type> & point<Type>::operator*=(const Type size) {
+point<Type> & point<Type>::operator*=(const Type size) 
+{
 	x *= size;
 	y *= size;
 	return *this;
 }
 
 template<typename Type>
-point<Type> point<Type>::operator*(const Type size) {
+point<Type> point<Type>::operator*(const Type size) const
+{
 	point temp = *this;
 	temp *= size;
 	return temp;
 }
 
 template<typename Type>
-Type point<Type>::operator*(const point other)
+Type point<Type>::operator*(const point & other) const
 {
 	return x * other.x + y * other.y;
+}
+
+template<typename Type>
+point<Type> & point<Type>::operator+=(const point & other)
+{
+	x += other.x;
+	y += other.y;
+	return *this;
+}
+
+template<typename Type>
+point<Type> point<Type>::operator+(const point & other) const
+{
+	point temp = *this;
+	temp += other;
+	return temp;
 }
 
 template<typename Type>
--- OpenGL/main.cpp
+++ OpenGL/main.cpp
@@ -16,32 +16,38 @@ void Init()
 	glClearColor(1, 1, 1, 1);
 }
 
+void DisplayCircle()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3i(0, 0, 0);
+	glPointSize(3);
+	GLint count = 1000;
+	point<GLfloat> Pt;
+	glBegin(GL_POINTS);
+	for (int i = 0; i < count; i++) {
+		Pt = Circle<GLfloat>(i, count)*0.5;
+		glVertex2f(Pt.x, Pt.y);
+	}
+	glEnd();
+	glFlush();
+}
+
 void Display()
 {
-	const DataType size = 0.1f;
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3d(0.0, 1.0, 1.0);	//Set Point Color	
+	const DataType step = 0.1f;
 	const GLint count = 10;
 	glPointSize(4.0f);
 	glLineWidth(2.0f);
-	glClear(GL_COLOR_BUFFER_BIT);
-	glColor3d(0.0, 1.0, 1.0);	//Set Point Color
-	/*glBegin(GL_POINTS);
-	point<GLfloat> Pt;
-	for (int i = 0; i < 1000; i++) {
-		Pt = Circle<GLfloat>(i, 1000);
-		glVertex2i(Pt.x, Pt.y);
-		glVertex2i(Pt.x - size, Pt.y + size);
-	}
-	glEnd();*/
 	for (int i = 0; i < count; i++) {
 		glColor3f(i*0.1f, i*0.1f, 1-i*0.1f);	//Change Color
-		point<DataType> Pt = Circle<DataType>(i, count);	//Find Circle center
-		Pt *= 0.8f;	//Shorten the radius
-	glBegin(i);
-		glVertex2f(Pt.x, Pt.y);	//Draw five point around the Circle
-		glVertex2f(Pt.x - size, Pt.y);	
-		glVertex2f(Pt.x, Pt.y + size);
-		glVertex2f(Pt.x + size, Pt.y);
-		glVertex2f(Pt.x, Pt.y - size);
+		point<DataType>Pt = Circle<DataType>(i, count) * 0.7f;
+		glBegin(i);
+		for (int j = 0; j < count; j++) {
+			Pt += Circle<DataType>(j, count) * 0.1f;
+			glVertex2f(Pt.x, Pt.y);
+		}
 		glEnd();
 	}
 	glFlush();
